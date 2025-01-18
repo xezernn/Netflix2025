1a9385803cf9b29b2e5a4bdfca92877edbf2170b
xezernn <xezernovruz79@gmail.com>
2025-01-18 14:58:27 +0400

    Bckend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,36 @@
 import axios from "axios"
-import { axiosInstance } from "./instance"
+// import { axiosInstance } from "./instance"
+// async function getData() {
+//     const res = await axiosInstance.get("/discover/movie?with_watch_providers=337&watch_region=US&language=en-US&sort_by=popularity.desc&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
+//     return res.data.results
+// }
+// async function getTv() {
+//     const res = await axiosInstance.get("/discover/tv?with_watch_providers=337&watch_region=US&language=en-US&sort_by=popularity.desc&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
+//     return res.data.results
+// }
+// async function topRatedMovies() {
+//     const res = await axiosInstance.get("/movie/top_rated?region=US&language=en-US&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
+//     return res.data.results
+// }
+// async function topRatedTv() {
+//     const res = await axiosInstance.get("/tv/top_rated?region=US&language=en-US&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
+//     return res.data.results
+// }
+
 async function getData() {
-    const res = await axiosInstance.get("/discover/movie?with_watch_providers=337&watch_region=US&language=en-US&sort_by=popularity.desc&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
+    const res = await axios.get("https://api.themoviedb.org/3/discover/movie?with_watch_providers=337&watch_region=US&language=en-US&sort_by=popularity.desc&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
     return res.data.results
 }
 async function getTv() {
-    const res = await axiosInstance.get("/discover/tv?with_watch_providers=337&watch_region=US&language=en-US&sort_by=popularity.desc&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
+    const res = await axios.get("https://api.themoviedb.org/3/discover/tv?with_watch_providers=337&watch_region=US&language=en-US&sort_by=popularity.desc&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
     return res.data.results
 }
 async function topRatedMovies() {
-    const res = await axiosInstance.get("/movie/top_rated?region=US&language=en-US&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
+    const res = await axios.get("https://api.themoviedb.org/3/movie/top_rated?region=US&language=en-US&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
     return res.data.results
 }
 async function topRatedTv() {
-    const res = await axiosInstance.get("/tv/top_rated?region=US&language=en-US&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
+    const res = await axios.get("https://api.themoviedb.org/3/tv/top_rated?region=US&language=en-US&api_key=35f3fea26d7c6bea37a8777ddbddbed3")
     return res.data.results
 }
 
